bbf97c3362aa8cce866ada93f6fe1e35c8a2aa9c
eclecticexistential <allofitbra@gmail.com>
2017-11-30 19:49:07 -0800

    Number to Word generator

---
+++ numtoword.js
@@ -0,0 +1,7 @@
+function numberToWord(a)
+    var b = a.toString();
+	  var numbers = ["one","two","three","four","five","six","seven","eight","nine"]
+	if(len(b)==1):
+		return numbers.a;
+
+print(numberToWord(1));
